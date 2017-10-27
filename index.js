542492aad96a4a11b30f45dbe313b1888e1e8cef
Won Hong <whong@outlook.com>
2017-10-26 21:26:57 -0400

    messaging feature completed

--- index.js
+++ index.js
@@ -1,50 +1,37 @@
-// app setup
 import express from 'express';
-let app = express();
-
-// log 4xx and 5xx responses to console
+import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import twilio from 'twilio';
+import 'dotenv/config'
+
+let app, twilioClient, isValid;
+
+// setup app and error logging
+app = express();
+app.use(bodyParser.json());
 app.use(morgan('dev', {
-  skip: function (req, res) { return res.statusCode < 400 }
+  skip: function (req, res) {
+    return res.statusCode < 400
+  }
 }));
 
-// set up daily logs
-import fs from 'fs';
-import path from 'path';
-import rfs from 'rfs';
+twilioClient = new twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
-let logDirectory = path.join(__dirname, 'log');
-fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+isValid = (req) => (
+  req.body.targetNumber.match(/^[0-9]{11}$/) &&
+  req.body.accessCode.length === 4 &&
+  req.header("secret-token") === process.env.SECRET_TOKEN
+);
 
-let accessLogStream = rfs('access.log', {
-  interval: '1d', // rotate daily
-  path: logDirectory
-});
-
-app.use(morgan('combined', {stream: accessLogStream}));
-
-// twilio setup
-import Twilio from 'twilio';
-import 'dotenv/config'
-let twilioClient = new Twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
-
-//app controller
-app.get('/', function (req, res) {
-  console.log(req);
-  console.log(process.env.TWILIO_SID);
-  res.send('Hello World!')
-});
-
+//controller
 app.post('/', function (req, res) {
-  console.log(req)
-  twilioClient.messages.create({
-    body: 'Hello from Node',
-    to: '+12345678901',  // Text this number
-    from: '+12345678901' // From a valid Twilio number
-  }).then();
+  isValid(req) && twilioClient.messages.create({
+    body: `Your CURE CONNECT access code is ${req.body.accessCode}`,
+    to: '+' + req.body.targetNumber,
+    from: process.env.TWILIO_MY_NUMBER
+  });
 });
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000!')
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
 
